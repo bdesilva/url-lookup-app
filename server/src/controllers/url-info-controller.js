be91f0862f49881462e2f263c6b15a317ae8006d
Benjamin De Silva <ben.p.desilva@gmail.com>
2016-12-15 00:49:26 -0800

    Started working on url parsing functionality.

--- server/src/controllers/url-info-controller.js
+++ server/src/controllers/url-info-controller.js
@@ -1,16 +1,51 @@
-/* BaseController class to handle url functionality */
+/* UrlInfoController class to handle url functionality */
 
 import BaseController from './base-controller';
 
 export class UrlInfoController extends BaseController {
     constructor(enforcer) {
         super();
-        if (enforcer != this.singletonEnforcer) throw "Cannot construct LoginController";
-        this.urlList = [];
+        if (enforcer != this.singletonEnforcer) throw "Cannot construct UrlInfoController";
+        this.urlList = {
+            'www.google.com': {
+                paths: ['/'],
+                query: ['']
+            },
+            'example.com': {
+                paths: ['/badurl'],
+                query: ''
+            },
+            'example.com:8400': {
+                paths: ['/hi'],
+                query: ''
+            }
+        };
     }
 
     getUrlData(params) {
-        console.dir(params);
-        return `Returned params: ${params.hostname_and_port}, ${params.original_path_and_query_string}`;
+        let result = {};
+        // return `Returned params: ${params.hostname_and_port}, ${params.original_path_and_query_string}`;
+        if (this.urlList[params.hostname_and_port]) {
+            const malUrlData = this.urlList[params.hostname_and_port];
+            if (params.original_path_and_query_string) {
+                //Split path and query from original data
+                const split = params.original_path_and_query_string.split('?');
+                const path = `/${split[0]}`;
+                const query = split[1];
+
+                //Analyze user path in depth by running a first pass to find an exact path                
+                if (malUrlData.paths.find(val => val === path))
+                    return this.buildOutputData(true, params);
+
+                //Next, if no match is found, start splitting the path in order to find a possible match on a substring
+                path.split('/').reduce((a, b) => {
+                    console.log(`prev: ${a}, next: ${b}`);
+                });
+            }
+        }
+    }
+
+    buildOutputData(isMalicious, params) {
+        return `${JSON.stringify(params)}: isMalicious: ${isMalicious}`;
     }
 }
--- server/src/router.js
+++ server/src/router.js
@@ -24,6 +24,6 @@ export class Router {
     //TODO: Enable POST route once CORS issue is fixed, revertign to GET for demo purposes.
     //this.router.post('/login', this.routes.login);
     this.router.get('/login/:username/:password', this.routes.login);
-    this.router.get('/url-info/:hostname_and_port/:original_path_and_query_string', this.routes.getUrlInfo);
+    this.router.get('/1/url-info/:hostname_and_port/:original_path_and_query_string', this.routes.getUrlInfo);
   }
 }
