e8647574bc83425e190d2b17a8f027c6104d974b
Benjamin De Silva <ben.p.desilva@gmail.com>
2016-12-16 00:59:09 -0800

    Fixed CORS issue

--- server/src/router.js
+++ server/src/router.js
@@ -22,8 +22,8 @@ export class Router {
   setupRoutes() {
     this.router.get('/', this.routes.index);
     //TODO: Enable POST route once CORS issue is fixed, revertign to GET for demo purposes.
-    //this.router.post('/login', this.routes.login);
-    this.router.get('/login/:username/:password', this.routes.login);
+    this.router.post('/login', this.routes.login);
+    // this.router.get('/login/:username/:password', this.routes.login);
     this.router.get('/1/url-info/:hostname_and_port/:original_path_and_query_string', this.routes.getUrlInfo);
     this.router.post('/1/url-info/', this.routes.postUrlInfo);
   }
