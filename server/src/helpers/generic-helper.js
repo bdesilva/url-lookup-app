6f6b7f7962309e8a69148c64e616e65af0f81522
Benjamin De Silva <ben.p.desilva@gmail.com>
2016-12-20 03:51:11 -0800

    Fixed small bug with query params splitting on Generic Helpers.

--- server/src/helpers/generic-helper.js
+++ server/src/helpers/generic-helper.js
@@ -3,7 +3,7 @@ export default class GenericHelpers {
         //Split path and query from original data
         const split = originalPathAndQueryString.split('?');
         const path = split[0].startsWith('/') ? `${split[0]}` : `/${split[0]}`;
-        const query = `?${split[1]}`;
+        const query = split[1] ? `?${split[1]}` : '';
         const fullPath = originalPathAndQueryString;
 
         return {path, query, fullPath};
